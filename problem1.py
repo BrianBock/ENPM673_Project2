a6433356aab2e97986a2de333b3d1fc14bc79130
Brian Bock <bbock@terpmail.umd.edu>
2020-02-28 11:58:05 -0500

    experimenting with Adrian Rosenbrock's gamma correction

--- problem1.py
+++ problem1.py
@@ -4,6 +4,8 @@ import numpy as np
 import cv2
 #import imutils
 import time
+# import argparse	
+		
 
 write_to_video=False
 
@@ -18,31 +20,78 @@ if write_to_video:
 	print("Writing to Video, Please Wait")
 
 # open the video specified by video_src
-video = cv2.VideoCapture('media/Night Drive - 2689.mp4') 
+video = cv2.VideoCapture('Problem1/media/Night Drive - 2689.mp4') 
 start_frame=0
 # move the video to the start frame and adjust the counter
 video.set(1,start_frame)
 count = start_frame
 
+
+
+#https://www.pyimagesearch.com/2015/10/05/opencv-gamma-correction/
+def adjust_gamma(image, gamma=1.0):
+	# build a lookup table mapping the pixel values [0, 255] to
+	# their adjusted gamma values
+	invGamma = 1.0 / gamma
+	table = np.array([((i / 255.0) ** invGamma) * 255
+		for i in np.arange(0, 256)]).astype("uint8")
+	# apply gamma correction using the lookup table
+	return cv2.LUT(image, table)
+		# equ = cv2.equalizeHist(highcontrast)
+
+
+
+
+
+
+
+
 while(video.isOpened()):
 	ret, frame = video.read() # ret is false if the video cannot be read
 	if ret:
 
-		# cv2.imshow("Original",frame)
-		# equ = cv2.equalizeHist(frame)
+		#cv2.imshow("Original",frame)
+		#highcontrast=frame*2
 
-		# cv2.imshow("Hist",equ)
+
+
+		#https://www.pyimagesearch.com/2015/10/05/opencv-gamma-correction/
+		# construct the argument parse and parse the arguments
+		# ap = argparse.ArgumentParser()
+		# ap.add_argument("-i", "--image", required=True,
+		# 	help="path to input image")
+		# args = vars(ap.parse_args())
+		# # load the original image
+		original = frame#cv2.imread(args["image"])
+
+		# loop over various values of gamma
+		for gamma in np.arange(0.0, 3.5, 0.5):
+			# ignore when gamma is 1 (there will be no change to the image)
+			if gamma == 1:
+				continue
+			# apply gamma correction and show the images
+			gamma = 2.5
+			adjusted = adjust_gamma(original, gamma=gamma)
+			# cv2.putText(adjusted, "g={}".format(gamma), (10, 30),
+			# 	cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+			cv2.imshow("Images", adjusted)
+			# cv2.waitKey(0)
+
+
+		# cv2.imshow("Contrast",equ)
+
+
 		# https://stackoverflow.com/questions/31998428/opencv-python-equalizehist-colored-image
-		img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		# img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-		# equalize the histogram of the Y channel
-		img_hsv[:,:,0] = cv2.equalizeHist(img_hsv[:,:,0])
+		# # equalize the histogram of the Y channel
+		# img_hsv[:,:,0] = cv2.equalizeHist(img_hsv[:,:,0])
 
-		# convert the YUV image back to RGB format
-		img_output = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
+		# # convert the YUV image back to RGB format
+		# img_output = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
 
-		cv2.imshow('Color input image', frame)
-		cv2.imshow('Histogram equalized', img_output)
+		# cv2.imshow('Color input image', frame)
+		# cv2.imshow('Histogram equalized', img_output)
 
 
 
