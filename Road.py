41b7c4377e5e8c4d2e94a18c5c1745c2d690e7de
Brian Bock <bbock@terpmail.umd.edu>
2020-03-06 09:22:56 -0500

    Method prototypes for road class

---
+++ Road.py
@@ -0,0 +1,25 @@
+class Road:
+	def __init__ (self):
+		pass
+
+	def compute_homography(self):
+		pass
+
+	def update_frame(self):
+		pass
+
+	def warp(self):
+		pass
+
+	def fill_contours(self):
+		pass
+
+	def find_peaks(self):
+		pass
+
+	def find_lane_line(self):
+		pass
+
+	def overlay(self):
+		pass
+		
